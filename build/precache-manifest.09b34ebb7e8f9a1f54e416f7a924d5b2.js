e6bb63397fdec4e649431805071aef55774b2606
EmmanuelSal <es1516@ic.ac.uk>
2021-06-03 11:01:25 +0100

    latest

---
+++ build/precache-manifest.09b34ebb7e8f9a1f54e416f7a924d5b2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f59302f5f96c69e2484",
+    "url": "/UGLearningDashboard/static/css/main.3af3de7a.chunk.css"
+  },
+  {
+    "revision": "1f59302f5f96c69e2484",
+    "url": "/UGLearningDashboard/static/js/main.1f59302f.chunk.js"
+  },
+  {
+    "revision": "2364b483ccab5ba6fd69",
+    "url": "/UGLearningDashboard/static/css/1.777e65b3.chunk.css"
+  },
+  {
+    "revision": "2364b483ccab5ba6fd69",
+    "url": "/UGLearningDashboard/static/js/1.2364b483.chunk.js"
+  },
+  {
+    "revision": "db8e1e48391a8b6eaf6c",
+    "url": "/UGLearningDashboard/static/js/runtime~main.db8e1e48.js"
+  },
+  {
+    "revision": "64db2776a5a125ba817dd03f5936f241",
+    "url": "/UGLearningDashboard/static/media/0.64db2776.jpg"
+  },
+  {
+    "revision": "60a859917fe65ea1f9f070e0dc080c76",
+    "url": "/UGLearningDashboard/static/media/shards-dashboards-logo.60a85991.svg"
+  },
+  {
+    "revision": "56606f8d4d205b498fe56fe1599d6602",
+    "url": "/UGLearningDashboard/static/media/1.56606f8d.jpg"
+  },
+  {
+    "revision": "e26f6eb5f904f9dcfadd704cc006ead3",
+    "url": "/UGLearningDashboard/static/media/2.e26f6eb5.jpg"
+  },
+  {
+    "revision": "1b203a8339857894a72e2e87e93cfb60",
+    "url": "/UGLearningDashboard/static/media/3.1b203a83.jpg"
+  },
+  {
+    "revision": "400aa6e4e41b111bfaee7cf020bb82da",
+    "url": "/UGLearningDashboard/index.html"
+  }
+];
